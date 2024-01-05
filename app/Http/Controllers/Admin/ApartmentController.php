913e532aca57a12adc3c4aa291f3225003c56f5b
luigitarallo <tarallo.gigi@gmail.com>
2024-01-05 19:39:46 +0100

    feat: add return view to index

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -1,20 +1,22 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ApartmentController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        // * TUTTI I RISTORANTI DEL TIPO
+        return view('admin.apartments.index');
     }
 
     /**
