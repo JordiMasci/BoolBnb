14a4f00bfb3eb907f9f1b336f38e43b283dcdec7
luigitarallo <tarallo.gigi@gmail.com>
2024-01-05 19:40:37 +0100

    feat: add template for test

---
+++ resources/views/admin/apartments/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2 class="fs-4 text-secondary my-4">
+        Questo è il tuo ristorante
+    </h2>
+    <div class="row justify-content-center">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">Ciao</div>
+
+                <div class="card-body">
+                  Ciao bello!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
